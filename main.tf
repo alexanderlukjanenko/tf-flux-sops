8659c2cacbdef76eefed93cc654841eb49b76e32
alexanderlukjanenko <alexander.lukjanenko@gmail.com>
2024-05-09 16:01:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,17 +43,17 @@ terraform {
   }
 }
 
-module "gke-workload-identity" {
-  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  use_existing_k8s_sa = true
-  name                = "kustomize-controller"
-  namespace           = "flux-system"
-  project_id          = var.GOOGLE_PROJECT
-  cluster_name        = "main"
-  location            = var.GOOGLE_REGION
-  annotate_k8s_sa     = true
-  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
-}
+#module "gke-workload-identity" {
+#  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#  use_existing_k8s_sa = true
+#  name                = "kustomize-controller"
+#  namespace           = "flux-system"
+#  project_id          = var.GOOGLE_PROJECT
+#  cluster_name        = "main"
+#  location            = var.GOOGLE_REGION
+#  annotate_k8s_sa     = true
+#  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+#}
 
 module "kms" {
   source             = "github.com/den-vasyliev/terraform-google-kms"
