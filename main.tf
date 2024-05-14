95fac9ca0d1a86897161d1f06908c5c0d692d194
alexanderlukjanenko <alexander.lukjanenko@gmail.com>
2024-05-14 19:08:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,23 +36,23 @@ terraform {
   }
 }
 
-#module "gke-workload-identity" {
-#  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-#  use_existing_k8s_sa = true
-#  name                = "kustomize-controller"
-#  namespace           = "flux-system"
-#  project_id          = var.GOOGLE_PROJECT
-#  cluster_name        = "main"
-#  location            = var.GOOGLE_REGION
-#  annotate_k8s_sa     = true
-#  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
-#}
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
 
-# module "kms" {
-#   source             = "github.com/den-vasyliev/terraform-google-kms"
-#   project_id         = var.GOOGLE_PROJECT
-#   keyring            = "sops-flux-t3"
-#   location           = "global"
-#   keys               = ["sops-key-flux-t3"]
-#   prevent_destroy    = false
-# }
+module "kms" {
+   source             = "github.com/den-vasyliev/terraform-google-kms"
+   project_id         = var.GOOGLE_PROJECT
+   keyring            = "sops-flux-t4"
+   location           = "global"
+   keys               = ["sops-key-flux-t4"]
+   prevent_destroy    = false
+}
